3ecb937e567e5b24e1b2ee26fcd7cf2364d2dbd1
Diego Costa <diego.szct@gmail.com>
2019-09-01 16:53:27 -0300

    Finishing the data input method

--- Algorithms/pre_processing.py
+++ Algorithms/pre_processing.py
@@ -1,9 +1,20 @@
 import pandas as pd
+import gensim
+
 
 def import_files (path, verbose = True):
     data = pd.read_csv(path)
+    
     if verbose is True:
-        print('Rows: {}, Columns: {}'.format(data.shape[0], data.shape[1])) 
+        print("Input Format:")
+        print('Rows: {}, Columns: {}\n'.format(data.shape[0], data.shape[1]))
+
+    data = data.dropna().reset_index(drop=True)
+    
+    if verbose is True:
+        print("Pre cleaning format:")
+        print('Rows: {}, Columns: {}'.format(data.shape[0], data.shape[1]))
+
     return data
 
     
