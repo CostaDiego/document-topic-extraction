2ba4aca9598899f25521f1d2cd18381deb0307f9
Diego Costa <diego.szct@gmail.com>
2019-09-04 04:32:56 -0300

    Implemented the method to make inferences

--- Algorithms/modelUsageAPI.py
+++ Algorithms/modelUsageAPI.py
@@ -1,4 +1,7 @@
 import pickle as pkl
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from gensim import corpora
 
 def save(obj, path, protocol = pkl.DEFAULT_PROTOCOL, verbose = False):
     if verbose:
@@ -32,4 +35,18 @@ def load(path, verbose = False):
     except pkl.PickleError:
         print('An Error occured during loading.')
 
-    
+def infer(document, model, dictionary, numb_topics = 16):
+    print("Loading necessary modules")
+
+    file = open(str(document), "r")
+    doc = file.read()
+    file.close()
+
+    mdl = load(model)
+    dicti = load(dictionary)
+
+    tokens = word_tokenize(doc)
+    topics = mdl.show_topics(formatted=True, num_topics = numb_topics, num_words = 20)
+    pd.DataFrame([(el[0], round(el[1],2), topics[el[0]][1]) 
+        for el in mdl[dicti.doc2bow(tokens)]],
+        columns=['topic #', 'weight', 'words in topic'])
