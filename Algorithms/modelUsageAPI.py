c55d56a8fcda10deda089ee501c04f44c06d520c
Diego Costa <diego.szct@gmail.com>
2019-09-04 04:51:17 -0300

    Lapidating the method to infer the topic

--- Algorithms/modelUsageAPI.py
+++ Algorithms/modelUsageAPI.py
@@ -35,18 +35,22 @@ def load(path, verbose = False):
     except pkl.PickleError:
         print('An Error occured during loading.')
 
-def infer(document, model, dictionary, numb_topics = 16):
-    print("Loading necessary modules")
-
+def infer(document, model, dictionary, numb_topics = 12):
+    print("Loading the Document from path: {}".format(str(document)))
     file = open(str(document), "r")
     doc = file.read()
     file.close()
 
+    print("Loading model from path: {}".format(str(model)))
     mdl = load(model)
+
+    print("Loading dictionary from path: {}".format(str(dictionary)))
     dicti = load(dictionary)
 
+    print("Inferring:\n")
     tokens = word_tokenize(doc)
     topics = mdl.show_topics(formatted=True, num_topics = numb_topics, num_words = 20)
-    pd.DataFrame([(el[0], round(el[1],2), topics[el[0]][1]) 
+    infer = pd.DataFrame([(el[0], round(el[1],2), topics[el[0]][1]) 
         for el in mdl[dicti.doc2bow(tokens)]],
         columns=['topic #', 'weight', 'words in topic'])
+    return infer
