774e1b79eb5d97fa580b9645f314255fbcbfc75f
Diego Costa <diego.szct@gmail.com>
2019-09-04 12:01:04 -0300

    Correct some prints

--- Algorithms/modelUsageAPI.py
+++ Algorithms/modelUsageAPI.py
@@ -36,21 +36,27 @@ def load(path, verbose = False):
         print('An Error occured during loading.')
 
 def infer(document, model, dictionary, numb_topics = 14):
-    print("Loading the Document from path: {}".format(str(document)))
-    file = open(str(document), "r")
-    doc = file.read()
-    file.close()
+    try:
+        print("Loading the Document from path: {}".format(str(document)))
+        file = open(str(document), "r")
+        doc = file.read()
+        file.close()
 
-    print("Loading model from path: {}".format(str(model)))
-    mdl = load(model)
+        print("Loading model from path: {}".format(str(model)))
+        mdl = load(model)
 
-    print("Loading dictionary from path: {}".format(str(dictionary)))
-    dicti = load(dictionary)
+        print("Loading dictionary from path: {}\n".format(str(dictionary)))
+        dicti = load(dictionary)
 
-    print("Inferring:\n")
-    tokens = word_tokenize(doc)
-    topics = mdl.show_topics(formatted=True, num_topics = numb_topics, num_words = 20)
-    infer = pd.DataFrame([(el[0], round(el[1],2), topics[el[0]][1]) 
-        for el in mdl[dicti.doc2bow(tokens)]],
-        columns=['topic #', 'weight', 'words in topic'])
-    return infer
+        print("Inferring:\n")
+        tokens = word_tokenize(doc)
+        topics = mdl.show_topics(formatted=True, num_topics = numb_topics, num_words = 20)
+        infer = pd.DataFrame([(el[0], round(el[1],2), topics[el[0]][1]) 
+            for el in mdl[dicti.doc2bow(tokens)]],
+            columns=['topic #', 'weight', 'words in topic'])
+        return infer
+    except pkl.PickleError:
+        print('An error occurred during the load of the binary files.')
+    
+    except IOError:
+        print('An error occurred during the load of the files.')
